bc91cadc2b6c5ce2d9544e29caf8c53d240f5590
quan0715 <60366187+quan0715@users.noreply.github.com>
2023-03-02 20:15:45 +0800

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='NotionBot',
+    version='0.0.1',
+    author='Quan',
+    author_email='quan787887@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/quan0715/PyNotion',
+    license='license.txt',
+    description='Wrapper around Notion API.',
+    # long_description=open('ReadMe.md').read(),
+    install_requires=[
+        "requests >= 2.18.4"
+    ],
+)
