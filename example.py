b30a4d4e1dbf12379749fbc67f95b3e297f298c6
quan0715 <60366187+quan0715@users.noreply.github.com>
2023-03-02 06:38:12 +0000

    example

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 from NotionClient import Notion
 
-
 def main():
     notion = Notion(auth="secret_8JtNxNiUCCWPRhFqzl1e2juzxoz96dyjYWubDLbNchy")
     # get notion user (for private)
@@ -9,5 +8,6 @@ def main():
     page_id = notion.search("Weakly Plan")['results'][0]['id']
     print(page_id)
     print(notion.search("XXX"))
+    
 if __name__ == "__main__":
     main()
